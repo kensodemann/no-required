e676cebef50110824e456f6f2b520afc9ab59639
Ken Sodemann <ken@ionic.io>
2024-08-05 15:00:33 -0500

    add android and form components

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,22 +13,59 @@
         </ion-toolbar>
       </ion-header>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-list>
+        <ion-item>
+          <ion-input required label="Name"></ion-input>
+        </ion-item>
+
+        <ion-item>
+          <ion-checkbox required :indeterminate="true">Vegan?</ion-checkbox>
+        </ion-item>
+
+        <ion-item>
+          <input type="checkbox" required />
+          <label for="scales">Vegan?</label>
+        </ion-item>
+
+        <ion-item>
+          <ion-select required label="Favorite Fruit" label-placement="stacked">
+            <ion-select-option value="apple">Apple</ion-select-option>
+            <ion-select-option value="banana">Banana</ion-select-option>
+            <ion-select-option value="orange">Orange</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item>
+          <select required>
+            <option value="apple">Apple</option>
+            <option value="banana">Banana</option>
+            <option value="orange">Orange</option>
+          </select>
+        </ion-item>
+
+        <ion-item>
+          <ion-radio-group required>
+            <ion-radio value="grapes">Grapes</ion-radio><br />
+            <ion-radio value="strawberries">Strawberries</ion-radio><br />
+            <ion-radio value="pineapple">Pineapple</ion-radio><br />
+            <ion-radio value="cherries">Cherries</ion-radio>
+          </ion-radio-group>
+        </ion-item>
+      </ion-list>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonCheckbox, IonContent, IonHeader, IonInput, IonItem, IonList, IonPage, IonRadio, IonRadioGroup, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/vue';
+
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +81,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
